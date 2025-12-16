22862d887c514ca3e322fa9ddbbe9e1b4b2889a7
Webmakxxx <webmakxxx@yandex.ru>
2025-12-16 06:33:35 +0300

    Add tour

---
+++ data.js
@@ -0,0 +1,232 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Ресепшен",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 3.125824764912931,
+        "pitch": 0,
+        "fov": 1.5651056597642057
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5284255441532908,
+          "pitch": 0.547767425689301,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.9401060423373497,
+          "pitch": 0.06501738866801787,
+          "rotation": 0,
+          "target": "3---"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.717648541925578,
+          "pitch": 0.3259782956679089,
+          "title": "Савита",
+          "text": "<div>Добро по жаловать в Савита:</div>| С - спортивная<div><div>| А - активная</div><div>| ВИТА - жизнь</div></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "Коридор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.054838207846076514,
+          "pitch": 0.2936452251919661,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "Кабинет Массажа",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.12448132587551441,
+        "pitch": 0.14904462280293984,
+        "fov": 1.5651056597642057
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18686829152630047,
+          "pitch": 0.3034916900405875,
+          "rotation": 12.566370614359176,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.1288678598882527,
+          "pitch": 0.20878252569908895,
+          "title": "Прессотерапия",
+          "text": "Аппаратный лимфодренажный массаж, при котором на тело человека воздействуют сжатым воздухом с помощью специального костюма."
+        },
+        {
+          "yaw": -2.6945029969345526,
+          "pitch": 0.0956335178282437,
+          "title": "<ya-tr-span data-index=\"66-0\" data-translated=\"true\" data-source-lang=\"en\" data-target-lang=\"ru\" data-value=\"Title\" data-translation=\"Название\" data-ch=\"0\" data-type=\"trSpan\" style=\"visibility: inherit !important;\">R-Sleek</ya-tr-span>",
+          "text": "<ya-tr-span data-index=\"68-0\" data-translated=\"true\" data-source-lang=\"en\" data-target-lang=\"ru\" data-value=\"Аппа\" data-translation=\"Аппа\" data-ch=\"0\" data-type=\"trSpan\" style=\"visibility: inherit !important;\">Аппарат премиум-класса для комплексного омоложения и коррекции фигуры, который сочетает лифтинг с эффективностью вакуумно-массажного воздействия. Он дает выраженный результат, но требует курсового применения и профессионального исполнения.</ya-tr-span>"
+        },
+        {
+          "yaw": -1.8015218688312267,
+          "pitch": 0.4044438585334351,
+          "title": "Биоимпедансометрия",
+          "text": "Быстрый, безболезненный и точный анализ состава тела (процент жира, мышц, жидкости и обмена веществ), что позволяет объективно оценить физическую форму, подобрать план питания и контролировать эффективность тренировок, выходя за рамки обычных весов."
+        }
+      ]
+    },
+    {
+      "id": "3---",
+      "name": "Раздевалки и залы",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 2.33164185344825,
+        "pitch": 0,
+        "fov": 1.5651056597642057
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9634035718090352,
+          "pitch": 0.4673624546071782,
+          "rotation": 6.283185307179586,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.46219568162937286,
+          "pitch": 0.19167530624742568,
+          "rotation": 12.566370614359176,
+          "target": "4--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--",
+      "name": "Женская раздевалка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Савита, НГУ им. П.Ф. Лесгафта",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
